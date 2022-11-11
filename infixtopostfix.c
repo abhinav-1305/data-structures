a644d77e7e241634e881e0a828b239cbb3638a12
Abhinav Kumar <113254225+abhinav-1305@users.noreply.github.com>
2022-11-11 18:51:39 +0530

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -4,118 +4,117 @@
 #define MAX 100
 
 char stack[MAX];
-char postfix[MAX],infix[MAX];
-int top=-1;
+char infix[MAX], postfix[MAX];
+int top = -1;
 
-int main(){
-    printf("enetr the infix expression: ");
-    gets(infix);
-intopost();   //calling the function to covert into infix expression to equivalent postfix expression
-print();
-return 0;
-}
+void push(char);
+char pop();
+int isEmpty();
+void inToPost();
+void print();
+int precedence(char);
 
-void intopost(){
-    int j=0;
-    char next;
-    char symbol;
-    for(int i=0;i<strlen(infix);i++){         //loop for reading all symbols of the loop
-      symbol = infix[i];
-
-      //if symbol is not a whitespace
-      if(!space(symbol))
-      {
-      switch(symbol)
-      {
-        case '(' :
-           push(symbol);
-           break;
-        case ')' :
-           while((next=pop()) !='(')
-           postfix[j++]=next;
-           break; 
-           default :
-           postfix[j++] = symbol;  
-
-           case '+' :
-           case '-' :
-           case '*' :
-           case '^' :
-
-           while(!isempty() && precedence (stack[top])>= precedence(symbol))
-           postfix[j++]=pop();
-           push(symbol);
-           break;
-           default :
-           postfix[j++]=symbol;
-      }
-    }
-}
-while(!isempty())
-postfix[j++]=pop();
-post[j]='\0';
-
-
-int space(char c)
+int main()
 {
-    if(c==' ' || c=='\t')
-    return 1;
-    else
+    printf("Enter the infix expression: ");
+    gets(infix);
+    inToPost();
+    print();
     return 0;
 }
 
-int precedence(char symbol){
-    switch(symbol){
-        case '*' :
-        return 3;
-        case '/' :
-        return 2;
-        case '+' :
-        case '-'  :
-        return 1;
+void inToPost()
+{
+    int i, j = 0;
+    char symbol, next;
+    for (i = 0; i < strlen(infix); i++)
+    {
+        symbol = infix[i];
+        switch (symbol)
+        {
+        case '(':
+            push(symbol);
+            break;
+        case ')':
+            while ((next = pop()) != '(')
+                postfix[j++] = next;
+            break;
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '^':
+            while (!isEmpty() && precedence(stack[top]) >= precedence(symbol))
+                postfix[j++] = pop();
+            push(symbol);
+            break;
         default:
+            postfix[j++] = symbol;
+        }
+    }
+
+    while (!isEmpty())
+        postfix[j++] = pop();
+    postfix[j] = '\0';
+}
+
+int precedence(char symbol)
+{
+    switch (symbol)
+    {
+    // Higher value means higher precedence
+    case '^':
+        return 3;
+    case '/':
+    case '*':
+        return 2;
+    case '+':
+    case '-':
+        return 1;
+    default:
         return 0;
     }
 }
 
-
-void print(){
-    int i=0;
-    printf("the equivalent postfix expression is: ");
-    while(postfix[i]){
-        printf("%c",postfix[i++]);
+void print()
+{
+    int i = 0;
+    printf("The equivalent postfix expression is: ");
+    while (postfix[i])
+    {
+        printf("%c", postfix[i++]);
     }
     printf("\n");
 }
 
-
-void push(cahr c)
+void push(char c)
 {
-    if(top==MAX-1){
-        printf("stack overflow!!!")
-        return; 
+    if (top == MAX - 1)
+    {
+        printf("Stack Overflow\n");
+        return;
     }
     top++;
-    stack(top)=c;
+    stack[top] = c;
 }
 
 char pop()
 {
-       char c;
-       if(top==-1){
-        printf("stack overflow!!!")
+    char c;
+    if (top == -1)
+    {
+        printf("Stack Underflow\n");
         exit(1);
-       }
-     c = stack[top];
-     top = top-1
-     return c;
+    }
+    c = stack[top];
+    top = top - 1;
+    return c;
 }
-
 
 int isEmpty()
 {
-    if(top==-1)
-    return 1;
-    else 
-    return 0;
+    if (top == -1)
+        return 1;
+    else
+        return 0;
 }
